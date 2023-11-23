0c83b2504e1c46a2f96b7d7d4e94a2367f73c745
LukeN25 <C22532639@mytudublin.ie>
2023-11-23 23:30:12 +0000

    MiniGame Beginning
    Man I love when the Dublin city works like it's supposed to

---
+++ Assets/MiniGame1/Scripts/AddButtons.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButtons : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+
+    private void Awake()
+    {
+        
+    }
+}
