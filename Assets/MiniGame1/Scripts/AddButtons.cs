5714c37a2cbba1d0d8e7525d11383fa76e157710
LukeN25 <C22532639@mytudublin.ie>
2023-11-24 12:53:02 +0000

    minigam in progress
    i love minigam, ples work

--- Assets/MiniGame1/Scripts/AddButtons.cs
+++ Assets/MiniGame1/Scripts/AddButtons.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class AddButtons : MonoBehaviour
 {
-    [SerializeField]
-    private Transform puzzleField;
+    [SerializeField] private Transform puzzleField;
+    [SerializeField] private GameObject btn;
 
     private void Awake()
     {
         
+        for(int i = 0; i < 12; i++) 
+        {
+            GameObject button = Instantiate(btn);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+
     }
 }
