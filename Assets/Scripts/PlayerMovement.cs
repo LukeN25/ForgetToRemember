c0a869bbfe7e90e959211aefef76bb44db5c69ff
Evan100354 <evanlong100354@gmail.com>
2023-12-07 17:22:04 +0000

    Started Dialogue system

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 8f;
+    private float speed = 3f;
     private float horizontal;
     private float vertical;
     public Animator animator;
