d76240a9afb1c1c7d89a9618e32c6c19594466ac
LukeN25 <C22532639@mytudublin.ie>
2023-12-08 12:45:04 +0000

    sped change

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float speed = 3f;
+    private float speed = 2f;
     private float horizontal;
     private float vertical;
     public Animator animator;
