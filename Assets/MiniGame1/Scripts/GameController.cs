f462cd2659d3192033fcdaf7c0766f24054dcbae
LukeN25 <C22532639@mytudublin.ie>
2023-12-08 14:23:18 +0000

    Memory game gud

--- Assets/MiniGame1/Scripts/GameController.cs
+++ Assets/MiniGame1/Scripts/GameController.cs
@@ -13,15 +13,27 @@ public class GameController : MonoBehaviour
     
     public List<Button> btns = new List<Button>();
 
+    private bool firstGuess, secondGuess;
+    
+    private int countGuesses;
+    private int countCorrectGuesses;
+    private int gameGuesses;
+
+    private int firstGuessIndex, secondGuessIndex;
+
+    private string firstGuessPuzzle, secondGuessPuzzle;
+
     void Awake()
     {
-        puzzles = Resources.LoadAll<Sprite>("Sprites/People");
+        puzzles = Resources.LoadAll<Sprite>("Sprites/Cards");
     }
 
     private void Start()
     {
         GetButtons();
         AddListeners();
+        AddGamePuzzles();
+        gameGuesses = gamePuzzles.Count / 2;
     }
 
     void GetButtons()
@@ -35,6 +47,22 @@ public class GameController : MonoBehaviour
         } 
     }
 
+    void AddGamePuzzles()
+    {
+        int looper = btns.Count;
+        int index = 0;
+
+        for (int i = 0;i < looper;i++) 
+        {
+            if(index == looper / 2)
+            {
+                index = 0;
+            }
+            gamePuzzles.Add(puzzles[index]);
+            index++;
+        }
+    }
+
     void AddListeners()
     {
         foreach (Button btn in btns)
@@ -44,8 +72,66 @@ public class GameController : MonoBehaviour
     }
 
     public void PickAPuzzle()
+    {        
+        if(!firstGuess) 
+        {
+            firstGuess = true;
+            firstGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            firstGuessPuzzle = gamePuzzles[firstGuessIndex].name;
+            btns[firstGuessIndex].image.sprite = gamePuzzles[firstGuessIndex];
+        }
+        else if(!secondGuess) 
+        {
+            secondGuess = true;
+            secondGuessIndex = int.Parse(UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name);
+            secondGuessPuzzle = gamePuzzles[secondGuessIndex].name;
+            btns[secondGuessIndex].image.sprite = gamePuzzles[secondGuessIndex];
+
+            StartCoroutine(CheckIfThePuzzlesMatch());
+
+            if(firstGuessPuzzle == secondGuessPuzzle) 
+            {
+                Debug.Log("Yahoo");
+            }
+            else
+            {
+                Debug.Log("Nayhoo");
+            }
+        }
+    }
+
+    IEnumerator CheckIfThePuzzlesMatch()
     {
-        string name = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject.name;
-        Debug.Log("Womp Womp " + name);
+        yield return new WaitForSeconds(1f);
+        if(firstGuessPuzzle == secondGuessPuzzle)
+        {
+            yield return new WaitForSeconds(.5f);
+            btns[firstGuessIndex].interactable = false;
+            btns[secondGuessIndex].interactable = false;
+
+            btns[firstGuessIndex].image.color = new Color(0, 0, 0, 0);
+            btns[secondGuessIndex].image.color = new Color(0, 0, 0, 0);
+            CheckIfTheGameIsFinished();
+        }
+        else
+        {
+            yield return new WaitForSeconds (.5f);
+            btns[firstGuessIndex].image.sprite = bgImage;
+            btns[secondGuessIndex].image.sprite = bgImage;
+        }
+
+        yield return new WaitForSeconds (.5f);
+
+        firstGuess = secondGuess = false;
+    }
+
+    void CheckIfTheGameIsFinished()
+    {
+        countCorrectGuesses++;
+        if(countCorrectGuesses == gameGuesses)
+        {
+            Debug.Log("Game Is Finished");
+            Debug.Log("It took you " + countGuesses + " many guess(es) to finish the game");
+        }
     }
 }
